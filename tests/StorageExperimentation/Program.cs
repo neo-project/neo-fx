c8b74df0cb0acec754c1cb92a119565a9e44bc9f
Harry Pierson <harrypierson@outlook.com>
2019-10-02 10:39:10 -0700

    add StorageExperimentation project + sample checkpoint files

---
+++ tests/StorageExperimentation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace StorageExperimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = File.Exists("./cp1/000033.sst");
+            Console.WriteLine($"{foo} {Environment.CurrentDirectory}");
+        }
+    }
+}
