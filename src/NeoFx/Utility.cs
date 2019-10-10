14fc7a9b5bc0654d174cf939ee1c0997a90d816a
Harry Pierson <harrypierson@outlook.com>
2019-10-10 13:44:19 -0700

    initial TryDecodePoint

--- src/NeoFx/Utility.cs
+++ src/NeoFx/Utility.cs
@@ -5,6 +5,7 @@ using System.Buffers;
 using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 using System.Security.Cryptography;
 
 namespace NeoFx
@@ -143,6 +144,270 @@ namespace NeoFx
 
             hash = default;
             return false;
+        }
+
+        static bool TestBit(this BigInteger bigInteger, int index)
+        {
+            return (bigInteger & (BigInteger.One << index)) > BigInteger.Zero;
+        }
+
+        static BigInteger Mod(this BigInteger x, BigInteger y)
+        {
+            x %= y;
+            if (x.Sign < 0)
+                x += y;
+            return x;
+        }
+
+        private static int BitLen(int w)
+        {
+            return (w < 1 << 15 ? (w < 1 << 7
+                ? (w < 1 << 3 ? (w < 1 << 1
+                ? (w < 1 << 0 ? (w < 0 ? 32 : 0) : 1)
+                : (w < 1 << 2 ? 2 : 3)) : (w < 1 << 5
+                ? (w < 1 << 4 ? 4 : 5)
+                : (w < 1 << 6 ? 6 : 7)))
+                : (w < 1 << 11
+                ? (w < 1 << 9 ? (w < 1 << 8 ? 8 : 9) : (w < 1 << 10 ? 10 : 11))
+                : (w < 1 << 13 ? (w < 1 << 12 ? 12 : 13) : (w < 1 << 14 ? 14 : 15)))) : (w < 1 << 23 ? (w < 1 << 19
+                ? (w < 1 << 17 ? (w < 1 << 16 ? 16 : 17) : (w < 1 << 18 ? 18 : 19))
+                : (w < 1 << 21 ? (w < 1 << 20 ? 20 : 21) : (w < 1 << 22 ? 22 : 23))) : (w < 1 << 27
+                ? (w < 1 << 25 ? (w < 1 << 24 ? 24 : 25) : (w < 1 << 26 ? 26 : 27))
+                : (w < 1 << 29 ? (w < 1 << 28 ? 28 : 29) : (w < 1 << 30 ? 30 : 31)))));
+        }
+
+        internal static int GetBitLength(this BigInteger i)
+        {
+            byte[] b = i.ToByteArray();
+            return (b.Length - 1) * 8 + BitLen(i.Sign > 0 ? b[b.Length - 1] : 255 - b[b.Length - 1]);
+        }
+
+        internal static int GetLowestSetBit(this BigInteger i)
+        {
+            if (i.Sign == 0)
+                return -1;
+            byte[] b = i.ToByteArray();
+            int w = 0;
+            while (b[w] == 0)
+                w++;
+            for (int x = 0; x < 8; x++)
+                if ((b[w] & 1 << x) > 0)
+                    return x + w * 8;
+            throw new Exception();
+        }
+
+        private static BigInteger[] FastLucasSequence(BigInteger p, BigInteger P, BigInteger Q, BigInteger k)
+        {
+            int n = k.GetBitLength();
+            int s = k.GetLowestSetBit();
+
+            Debug.Assert(k.TestBit(s));
+
+            BigInteger Uh = 1;
+            BigInteger Vl = 2;
+            BigInteger Vh = P;
+            BigInteger Ql = 1;
+            BigInteger Qh = 1;
+
+            for (int j = n - 1; j >= s + 1; --j)
+            {
+                Ql = (Ql * Qh).Mod(p);
+
+                if (k.TestBit(j))
+                {
+                    Qh = (Ql * Q).Mod(p);
+                    Uh = (Uh * Vh).Mod(p);
+                    Vl = (Vh * Vl - P * Ql).Mod(p);
+                    Vh = ((Vh * Vh) - (Qh << 1)).Mod(p);
+                }
+                else
+                {
+                    Qh = Ql;
+                    Uh = (Uh * Vl - Ql).Mod(p);
+                    Vh = (Vh * Vl - P * Ql).Mod(p);
+                    Vl = ((Vl * Vl) - (Ql << 1)).Mod(p);
+                }
+            }
+
+            Ql = (Ql * Qh).Mod(p);
+            Qh = (Ql * Q).Mod(p);
+            Uh = (Uh * Vl - Ql).Mod(p);
+            Vl = (Vh * Vl - P * Ql).Mod(p);
+            Ql = (Ql * Qh).Mod(p);
+
+            for (int j = 1; j <= s; ++j)
+            {
+                Uh = Uh * Vl * p;
+                Vl = ((Vl * Vl) - (Ql << 1)).Mod(p);
+                Ql = (Ql * Ql).Mod(p);
+            }
+
+            return new BigInteger[] { Uh, Vl };
+        }
+
+        internal static BigInteger NextBigInteger(this Random rand, int sizeInBits)
+        {
+            if (sizeInBits < 0)
+                throw new ArgumentException("sizeInBits must be non-negative");
+            if (sizeInBits == 0)
+                return 0;
+            byte[] b = new byte[sizeInBits / 8 + 1];
+            rand.NextBytes(b);
+            if (sizeInBits % 8 == 0)
+                b[b.Length - 1] = 0;
+            else
+                b[b.Length - 1] &= (byte)((1 << sizeInBits % 8) - 1);
+            return new BigInteger(b);
+        }
+
+        static bool TrySqrt(BigInteger value, BigInteger q, out BigInteger result)
+        {
+            static bool TryCheckSqrt(BigInteger z, BigInteger q, BigInteger value, out BigInteger result)
+            {
+                if (Mod(z * z, q) == value)
+                {
+                    result = z;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    return false;
+                }
+            }
+
+            if (q.TestBit(1))
+            {
+                var sqrt = BigInteger.ModPow(value, (q >> 2) + BigInteger.One, q);
+                return TryCheckSqrt(sqrt, q, value, out result);
+            }
+
+            BigInteger qMinusOne = q - BigInteger.One;
+            BigInteger legendreExponent = qMinusOne >> 1;
+            if (BigInteger.ModPow(value, legendreExponent, q) != 1)
+            {
+                result = default;
+                return false;
+
+            }
+            BigInteger u = qMinusOne >> 2;
+            BigInteger k = (u << 1) + 1;
+            BigInteger Q = value;
+            BigInteger fourQ = Mod((Q << 2), q);
+            BigInteger U, V;
+            do
+            {
+                Random rand = new Random();
+                BigInteger P;
+                do
+                {
+                    P = rand.NextBigInteger(q.GetBitLength());
+                }
+                while (P >= q || BigInteger.ModPow(P * P - fourQ, legendreExponent, q) != qMinusOne);
+                BigInteger[] resultprime = FastLucasSequence(q, P, Q, k);
+                U = resultprime[0];
+                V = resultprime[1];
+                if (Mod(V * V, q) == fourQ)
+                {
+                    if (V.TestBit(0))
+                    {
+                        V += q;
+                    }
+                    V >>= 1;
+                    Debug.Assert((V * V).Mod(q) == value);
+
+                    result = V;
+                    return true;
+                }
+            }
+            while (U.Equals(BigInteger.One) || U.Equals(qMinusOne));
+
+            result = default;
+            return false;
+
+        }
+
+        private static bool TryToByteArray(this BigInteger bigInteger, int expectedLength, [NotNullWhen(true)] out byte[]? buffer)
+        {
+            var array = new byte[expectedLength];
+            var byteCount = bigInteger.GetByteCount(true);
+
+            if (byteCount <= expectedLength
+                && bigInteger.TryWriteBytes(array.AsSpan().Slice(expectedLength - byteCount), out var written, true, true))
+            {
+                Debug.Assert(written == byteCount);
+                buffer = array;
+                return true;
+            }
+
+            buffer = null;
+            return false;
+        }
+
+        private static bool TryDecompressPoint(ReadOnlySpan<byte> xSpan, int yTilde, int expectedLength, ECCurve curve, out ECPoint point)
+        {
+            Debug.Assert(curve.IsExplicit);
+
+            var x = new BigInteger(xSpan, isUnsigned: true, isBigEndian: true);
+            var a = new BigInteger(curve.A, isUnsigned: true, isBigEndian: true);
+            var b = new BigInteger(curve.B, isUnsigned: true, isBigEndian: true);
+            var prime = new BigInteger(curve.Prime, isUnsigned: true, isBigEndian: true);
+
+            var alpha = Mod((x * ((x * x) + a)) + b, prime);
+            if (TrySqrt(alpha, prime, out var beta))
+            {
+                if ((beta.IsEven ? 0 : 1) != yTilde)
+                {
+                    beta = prime - beta;
+                }
+
+                if (x.TryToByteArray(expectedLength, out var xArray)
+                    && beta.TryToByteArray(expectedLength, out var betaArray))
+                {
+                    point = new ECPoint()
+                    {
+                        X = xArray,
+                        Y = betaArray
+                    };
+                    return true;
+                }
+            }
+
+            point = default;
+            return false;
+        }
+
+        public static bool TryDecodePoint(ReadOnlySpan<byte> span, ECCurve curve, out ECPoint point)
+        {
+            if (!curve.IsExplicit && curve.IsNamed)
+            {
+                using var ecdsa = ECDsa.Create(curve);
+                curve = ecdsa.ExportExplicitParameters(false).Curve;
+            }
+            var prime = new BigInteger(curve.Prime, isUnsigned: true, isBigEndian: true);
+            int expectedLength = (prime.GetBitLength() + 7) / 8;
+
+            switch (span[0])
+            {
+                case 0x02:
+                case 0x03:
+                    Debug.Assert(span.Length == expectedLength + 1);
+                    int yTilde = span[0] & 1;
+                    return TryDecompressPoint(span.Slice(1), yTilde, expectedLength, curve, out point);
+                case 0x04:
+                case 0x06:
+                case 0x07:
+                    Debug.Assert(span.Length == (2 * expectedLength) + 1);
+                    point = new ECPoint()
+                    {
+                        X = span.Slice(1, expectedLength).ToArray(),
+                        Y = span.Slice(1 + expectedLength, expectedLength).ToArray()
+                    };
+                    return true;
+                default:
+                    point = default;
+                    return false;
+            }
         }
     }
 }
---
+++ tests/NeoFxTests/ECPointTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace NeoFxTests
+{
+    public class ECPointTests
+    {
+        static string[] testValues = new string[] {
+            // Neo 2 mainnet standby validators
+            "03b209fd4f53a7170ea4444e0cb0a6bb6a53c2bd016926989cf85f9b0fba17a70c",
+            "02df48f60e8f3e01c48ff40b9b7f1310d7a8b2a193188befe1c2e3df740e895093",
+            "03b8d9d5771d8f513aa0869b9cc8d50986403b78c6da36890638c3d46a5adce04a",
+            "02ca0e27697b9c248f6f16e085fd0061e26f44da85b58ee835c110caa5ec3ba554",
+            "024c7b7fb6c310fccf1ba33b082519d82964ea93868d676662d4a59ad548df0e7d",
+            "02aaec38470f6aad0042c6e877cfd8087d2676b0f516fddd362801b9bd3936399e",
+            "02486fd15702c4490a26703112a5cc1d0923fd697a33406bd5a1c00e0013b09a70",
+
+            // Neo 2 testnet standby validators
+            "0327da12b5c40200e9f65569476bbff2218da4f32548ff43b6387ec1416a231ee8",
+            "026ce35b29147ad09e4afe4ec4a7319095f08198fa8babbe3c56e970b143528d22",
+            "0209e7fd41dfb5c2f8dc72eb30358ac100ea8c72da18847befe06eade68cebfcb9",
+            "039dafd8571a641058ccc832c5e2111ea39b09c0bde36050914384f7a48bce9bf9",
+            "038dddc06ce687677a53d54f096d2591ba2302068cf123c1f2d75c2dddc5425579",
+            "02d02b1873a0863cd042cc717da31cea0d7cf9db32b74d4c72c01b0011503e2e22",
+            "034ff5ceeac41acf22cd5ed2da17a6df4dd8358fcb2bfb1a43208ad0feaab2746b",
+
+            //// Neo 3 testnet standby validators
+            "023e9b32ea89b94d066e649b124fd50e396ee91369e8e2a6ae1b11c170d022256d",
+            "03009b7540e10f2562e5fd8fac9eaec25166a58b26e412348ff5a86927bfac22a2",
+            "02ba2c70f5996f357a43198705859fae2cfea13e1172962800772b3d588a9d4abd",
+            "03408dcd416396f64783ac587ea1e1593c57d9fea880c8a6a1920e92a259477806",
+            "02a7834be9b32e2981d157cb5bbd3acb42cfd11ea5c3b10224d7a44e98c5910f1b",
+            "0214baf0ceea3a66f17e7e1e839ea25fd8bed6cd82e6bb6e68250189065f44ff01",
+            "030205e9cefaea5a1dfc580af20c8d5aa2468bb0148f1a5e4605fc622c80e604ba"
+        };
+
+        static bool TryHexToBytes(ReadOnlySpan<char> hex, Span<byte> bytes)
+        {
+            if (bytes.Length >= hex.Length / 2)
+            {
+                for (int i = 0; i < hex.Length; i += 2)
+                {
+                    bytes[i / 2] = byte.Parse(hex.Slice(i, 2), System.Globalization.NumberStyles.HexNumber);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+
+        [Fact]
+        public void test()
+        {
+            var buffer = new byte[testValues[0].Length / 2];
+            foreach (var test in testValues)
+            {
+                TryHexToBytes(test, buffer).Should().BeTrue();
+
+                var neoPoint = Neo.Cryptography.ECC.ECPoint.DecodePoint(buffer, Neo.Cryptography.ECC.ECCurve.Secp256r1);
+                var neoPublicKey = neoPoint.EncodePoint(false).AsSpan().Slice(1);
+
+                NeoFx.Utility.TryDecodePoint(buffer, ECCurve.NamedCurves.nistP256, out var fxPoint).Should().BeTrue();
+
+                neoPublicKey.Slice(0, 32).SequenceEqual(fxPoint.X).Should().BeTrue();
+                neoPublicKey.Slice(32, 32).SequenceEqual(fxPoint.Y).Should().BeTrue();
+            }
+        }
+
+    }
+}
