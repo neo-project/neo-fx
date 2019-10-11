8d81969f6adc2497cdd0b08fb2bbdcde90bb64b3
Harry Pierson <harrypierson@outlook.com>
2019-10-11 12:13:11 -0700

    move span writer to separate file

--- src/NeoFx/Storage/BinaryFormat.TryWrite.cs
+++ src/NeoFx/Storage/BinaryFormat.TryWrite.cs
@@ -7,28 +7,6 @@ using System.Text;
 
 namespace NeoFx.Storage
 {
-    public ref struct SpanWriter<T>
-    {
-        private readonly Span<T> span;
-        private int position;
-
-        public SpanWriter(Span<T> span)
-        {
-            this.span = span;
-            position = 0;
-        }
-
-        public Span<T> Span => span.Slice(position);
-        public ReadOnlySpan<T> Contents => span.Slice(0, position);
-
-        public int Length => span.Length - position;
-
-        public void Advance(int size)
-        {
-            position += size;
-        }
-    }
-
     public static partial class BinaryFormat
     {
         public static bool TryWriteBytes(this CoinReference input, Span<byte> span)
---
+++ src/NeoFx/Storage/SpanWriter.cs
@@ -0,0 +1,31 @@
+﻿using NeoFx.Models;
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NeoFx.Storage
+{
+    public ref struct SpanWriter<T>
+    {
+        private readonly Span<T> span;
+        private int position;
+
+        public SpanWriter(Span<T> span)
+        {
+            this.span = span;
+            position = 0;
+        }
+
+        public Span<T> Span => span.Slice(position);
+        public ReadOnlySpan<T> Contents => span.Slice(0, position);
+
+        public int Length => span.Length - position;
+
+        public void Advance(int size)
+        {
+            position += size;
+        }
+    }
+}
