38778bee3da0489792fb001e900c18b464818a4b
Harry Pierson <harrypierson@outlook.com>
2020-01-28 11:36:08 -0800

    initial msg WIP

---
+++ src/P2P/Messages/GetAddrMessage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NeoFx.P2P.Messages
+{
+    public sealed class GetAddrMessage : Message
+    {
+        public const string CommandText = "getaddr";
+
+        public GetAddrMessage(in MessageHeader header) : base(header)
+        {
+        }
+
+        public override void LogMessage(ILogger logger)
+        {
+            logger.LogInformation("Receive {messageType}",
+                nameof(GetAddrMessage));
+        }
+    }
+}
